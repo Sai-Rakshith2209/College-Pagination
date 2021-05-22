57b50f946b3118462715072351f6e1d6c1bbecb7
Sai Rakshith <sairakshith10@gmail.com>
2021-05-22 11:04:56 +0530

    Pagination-Module-added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import logo from "./logo.svg";
+
+import axios from "axios";
+import "./App.css";
+import { Posts } from "./components/Posts";
+import { Pagination } from "./components/Pagination";
 
 function App() {
+  const [dekhocollegeposts, setPosts] = useState([]);
+  const [collageLoader, setcollageLoader] = useState(false);
+  const [collageCurrentPage, setcollageCurrentPage] = useState(1);
+  const [collegeDekhopostsPerPage] = useState(10);
+  useEffect(() => {
+    const fetchCollegeDekhoPosts = async () => {
+      setcollageLoader(true);
+      const res = await axios.get("http://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setcollageLoader(false);
+    };
+    fetchCollegeDekhoPosts();
+  }, []);
+
+  // Get Current Posts
+
+  const indexOfLastPost = collageCurrentPage * collegeDekhopostsPerPage;
+  const indexOfFirstPost = indexOfLastPost - collegeDekhopostsPerPage;
+  const currentPosts = dekhocollegeposts.slice(
+    indexOfFirstPost,
+    indexOfLastPost
+  );
+
+  // Paginate Change Page Function
+
+  const paginate = (collagePageNumber) =>
+    setcollageCurrentPage(collagePageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My College Dekho Pagination Logic</h1>
+      <Posts posts={currentPosts} collageLoader={collageLoader} />
+      <Pagination
+        postsPerPage={collegeDekhopostsPerPage}
+        totalPosts={dekhocollegeposts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
